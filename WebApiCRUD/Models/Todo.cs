434684b89d8424c90ef23d1f04c28b177d2e9813
Krzysztof Królikowski <kkrolikowski92@gmail.com>
2019-02-08 21:43:12 +0100

    add todos module with dbcontext

---
+++ WebApiCRUD/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCRUD.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+
+    }
+}
