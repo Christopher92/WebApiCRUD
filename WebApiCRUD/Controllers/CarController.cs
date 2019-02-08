aaccd681281ea301d035a917fd432e6cdd159197
Krzysztof Królikowski <K.Krolikowski@wasko.pl>
2019-02-08 14:12:35 +0100

    add dummy CRUD in car controller

---
+++ WebApiCRUD/Controllers/CarController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    public class CarController : Controller
+    {
+        // GET
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var car = new Car()
+            {
+                Id = id,
+                Name = "Ferrari",
+                Description = "Super fast car"
+            };
+
+            return Ok(car);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Add([FromBody] Car model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction("Add", new { id = 5 });
+        }
+
+        // PUT
+        [HttpPut]
+        public IActionResult Put([FromBody] Car model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction("Put", new { id = model.Id });
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();     //204 - successfull, but no content
+            //return Ok(new {id = 5});
+        }
+    }
+}
---
+++ WebApiCRUD/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiCRUD.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        [MinLength(5)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
